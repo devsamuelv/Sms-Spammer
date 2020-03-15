82b01d2f51ecbe8ce1beabf9ca2614221a9d9f67
DevSamuelV <realalloutmusic@gmail.com>
2020-03-14 20:03:43 -0400

    All Main Features And Github Is Working

---
+++ app/src/main/java/com/devsamuel/spam/SplashScreen.java
@@ -0,0 +1,16 @@
+package com.devsamuel.spam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
